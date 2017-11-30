142a531c9db0b6b01b8c67ff1ef84e7b8f20c39e
kartikwar <kartiksirwani@gmail.com>
2017-11-30 15:23:09 +0530

    k folds added in first level of learning due to limited data

--- titanic_survivors_determination.py
+++ titanic_survivors_determination.py
@@ -10,6 +10,7 @@ import xgboost as xgb
 import re
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import KFold;
 
 
 def label_encode_features(dataframe):
@@ -47,6 +48,30 @@ def get_name_length(name):
 	length = len(name)
 	return length    
 
+def get_oof(clf, x_train, y_train, x_test):
+	# print (type(x_train))
+	ntrain = x_train.shape[0]
+	# print ntrain
+	ntest = x_test.shape[0]
+	oof_train = np.zeros((ntrain,))
+	oof_test = np.zeros((ntest,))
+	SEED = 0 # for reproducibility
+	NFOLDS = 5 # set folds for out-of-fold prediction
+	kf = KFold(len(x_train), n_folds= NFOLDS, random_state=SEED)
+	oof_test_skf = np.empty((NFOLDS, ntest))
+	# print oof_test_skf.shape 
+
+	for i, (train_index, test_index) in enumerate(kf):
+		x_tr = x_train.iloc[train_index]
+		y_tr = y_train.iloc[train_index]
+		x_te = x_train.iloc[test_index]
+		clf.fit(x_tr, y_tr)
+		oof_train[test_index] = clf.predict(x_te)
+		oof_test_skf[i, :] = clf.predict(x_test)
+
+	oof_test[:] = oof_test_skf.mean(axis=0)	
+	return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+	
 def feature_engineering(training_set, predict_set):
 	full_set = [training_set, predict_set]
 	for dataset in full_set:
@@ -74,14 +99,17 @@ def feature_engineering(training_set, predict_set):
 		dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
 		dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
 		dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
- 		
+ 		# Sex
+ 		dataset.Sex.fillna('0', inplace=True)
+		dataset.loc[dataset.Sex != 'male', 'Sex'] = 0
+		dataset.loc[dataset.Sex == 'male', 'Sex'] = 1
+
 		dataset = dataset.drop(drop_elements, axis = 1)	
 	return (training_set, predict_set)	
 
 def data_preprocessing():
 	training_set = pd.read_csv('past_data_titanic.csv')
 	predict_set = pd.read_csv('test_data_titanic.csv')
-	# print((list(predict_set['PassengerId'])))
 
 	training_set_X = training_set.drop('Survived', axis=1)
 	training_set_Y = training_set['Survived']
@@ -91,23 +119,15 @@ def data_preprocessing():
 
 	training_set_X, predict_set  = feature_engineering(training_set_X, predict_set)
 
-	X_train, X_test, y_train, y_test = train_test_split(training_set_X, training_set_Y, random_state = 0)	
+	# X_train, X_test, y_train, y_test = train_test_split(training_set_X, training_set_Y, random_state = 0)
+	X_train = training_set_X.copy()
+	y_train = training_set_Y.copy()	
 	
 	corelations = find_corelations_for_survival(X_train, y_train)
-	# print corelations
-
-	# selected_features = feature_engineering(corelations)
-	# X_train = X_train[selected_features]
-	# X_test = X_test[selected_features]
 
 	X_train = label_encode_features(X_train)
-	# print(X_train.columns)
-	X_test = label_encode_features(X_test)
-	# print(X_test.columns)
-	# X_train = pca_feature_engineering(X_train)
-	# X_test = pca_feature_engineering(X_test)
-
-	return (X_train, X_test, y_train, y_test, predict_set)
+	
+	return (X_train, y_train, predict_set)
 
 def determine_best_params_random_forest(X_train, y_train):
 	grid_values = {'n_estimators' : [1, 5, 25],
@@ -120,70 +140,36 @@ def determine_best_params_random_forest(X_train, y_train):
 	best_params =grid_clf_accuracy.best_params_
 	return best_params	
 
-def first_level_training(X_train, y_train, X_test, y_test, predict_set):
+def first_level_training(X_train, y_train, predict_set):
 	rf = RandomForestClassifier(warm_start=True,  n_jobs=-1 , verbose=0,
 		min_samples_leaf=2, n_estimators=500, max_features='sqrt',
-		max_depth=6, random_state = 0).fit(X_train, y_train)
-	rf_predict_train = rf.predict(X_train)
-	rf_predict_test = rf.predict(X_test)
-	rf_predict_set = rf.predict(predict_set)
-	rf_predict_set = rf_predict_set.reshape(418,1)
-	rf_predict_train = rf_predict_train.reshape(668, 1)
-	rf_predict_test = rf_predict_test.reshape(223,1)
-
+		max_depth=6, random_state = 0)
+	rf_predict_train, rf_predict_test = get_oof(rf, X_train, y_train, predict_set)
 
 	et = ExtraTreesClassifier(n_estimators=500,  n_jobs=-1 , verbose=0,
-		min_samples_leaf=2, max_depth=8, random_state=0).fit(X_train, y_train)
-	et_predict_train = et.predict(X_train)
-	et_predict_test = et.predict(X_test)
-	et_predict_train = et_predict_train.reshape(668, 1)
-	et_predict_test = et_predict_test.reshape(223,1)
-	et_predict_set = et.predict(predict_set)
-	et_predict_set = et_predict_set.reshape(418,1)
+		min_samples_leaf=2, max_depth=8, random_state=0)
+	et_predict_train, et_predict_test = get_oof(et, X_train, y_train, predict_set)
 
 	ada = AdaBoostClassifier(n_estimators= 500
-		, learning_rate=0.75, random_state=0).fit(X_train, y_train)	
-	ada_predict_train = ada.predict(X_train)
-	ada_predict_test = ada.predict(X_test)
-	ada_predict_train = ada_predict_train.reshape(668, 1)
-	ada_predict_test = ada_predict_test.reshape(223,1)
-	ada_predict_set = ada.predict(predict_set)
-	ada_predict_set = ada_predict_set.reshape(418,1)
+		, learning_rate=0.75, random_state=0)
+	ada_predict_train, ada_predict_test = get_oof(ada, X_train, y_train, predict_set)		
 
 	gb = GradientBoostingClassifier(n_estimators=500, verbose= 0
-		, max_depth= 5, min_samples_leaf=2 ,random_state=0).fit(X_train, y_train)
-	gb_predict_train = gb.predict(X_train)
-	gb_predict_test = gb.predict(X_test)
-	gb_predict_train = gb_predict_train.reshape(668, 1)
-	gb_predict_test = gb_predict_test.reshape(223,1)
-	gb_predict_set = gb.predict(predict_set)
-	gb_predict_set = gb_predict_set.reshape(418,1)
+		, max_depth= 5, min_samples_leaf=2 ,random_state=0)
+	gb_predict_train, gb_predict_test = get_oof(gb, X_train, y_train, predict_set)	
 
-	svc = SVC(kernel='linear', C=0.025, random_state=0).fit(X_train, y_train)
-	svc_predict_train = svc.predict(X_train)
-	svc_predict_test = svc.predict(X_test)
-	svc_predict_train = svc_predict_train.reshape(668, 1)
-	svc_predict_test = svc_predict_test.reshape(223,1)
-	svc_predict_set = svc.predict(predict_set)
-	svc_predict_set = svc_predict_set.reshape(418,1)
+	svc = SVC(kernel='linear', C=0.025, random_state=0)
+	svc_predict_train, svc_predict_test = get_oof(svc, X_train, y_train, predict_set)
 
-
-	knn = KNeighborsClassifier(n_neighbors=1).fit(X_train, y_train)
-	knn_predict_train = knn.predict(X_train)
-	knn_predict_test = knn.predict(X_test)
-	knn_predict_set = knn.predict(predict_set)
-	knn_predict_train = knn_predict_train.reshape(668, 1)
-	knn_predict_test = knn_predict_test.reshape(223, 1)
-	knn_predict_set = knn_predict_set.reshape(418,1) 
-
+	knn = KNeighborsClassifier(n_neighbors=1)
+	knn_predict_train, knn_predict_test = get_oof(knn, X_train, y_train, predict_set) 
 	
 	X_train = np.concatenate(( rf_predict_train, et_predict_train, ada_predict_train, 
 		gb_predict_train, svc_predict_train, knn_predict_train), axis=1)
-	X_test = np.concatenate(( rf_predict_test, et_predict_test, 
+	predict_set = np.concatenate(( rf_predict_test, et_predict_test, 
 		ada_predict_test, gb_predict_test, svc_predict_test, knn_predict_test), axis=1)
-	predict_set = np.concatenate(( rf_predict_set, et_predict_set, 
-		ada_predict_set, gb_predict_set, svc_predict_set, knn_predict_set ), axis=1)		
-	return (X_train, X_test, predict_set)	
+						
+	return (X_train, predict_set)	
 
 def second_level_training(X_train, y_train):
 	gbm = xgb.XGBClassifier(n_estimators= 2000,max_depth= 4,min_child_weight= 2,
@@ -211,14 +197,14 @@ def save_to_csv(dataset, survival_predictions, file_name):
 	return None	
 
 if __name__ == '__main__':
-	X_train, X_test, y_train, y_test, predict_set =  data_preprocessing()
+	X_train, y_train, predict_set =  data_preprocessing()
 	predict_X = label_encode_features(predict_set)
-	X_train, X_test, predict_X =  first_level_training(X_train, y_train, X_test, 
-		y_test, predict_X)
+	X_train, predict_X =  first_level_training(X_train, y_train, predict_X)
+	# get_oof(RandomForestClassifier() ,X_train, y_train, predict_X)
 	clf = second_level_training(X_train, y_train)
-	training_accuracy, test_accuracy = find_accuracy_of_model(clf, 
-		X_train, X_test, y_train, y_test)
-	print(training_accuracy, test_accuracy)
+	# training_accuracy, test_accuracy = find_accuracy_of_model(clf, 
+	# 	X_train, X_test, y_train, y_test)
+	# print(training_accuracy, test_accuracy)
 	survival_predictions = clf.predict(predict_X)
 	save_to_csv(predict_set, survival_predictions, 'predictions.csv')
 	
